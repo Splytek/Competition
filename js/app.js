d37db1b2ff6d3a979c286e6b983f1a99cb182f59
Hugo Emmanuel Gonzalez Gomez <splytek@gmail.com>
2021-08-21 15:22:45 -0500

    Mi Primer Version

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function mostrarDatos(){
+
+    //var numeroBinarioTxt = document.getElementById("numero-Binario").value; //obtenemos el valor del input
+
+    var numeroBinarioTxt = document.getElementById('numero-Binario').value;
+
+    var texto = numeroBinarioTxt;
+
+    var Hola = texto += texto;
+
+    document.getElementById("mostrarTexto-Binarios").innerHTML = Hola + '\n';
+    
+    
+/*     if(numeroBinarioText.length <= 15){
+      var texto = numeroBinarioText;
+      document.getElementById("mostrarTexto-Binarios").innerHTML = texto;
+    } else {
+      alert('Solo se permiten 15 numeros');    
+    } */
+
+
+/*     for(i=0; i<10; 1++){
+      if(){
+
+      }
+    } */
+
+}
