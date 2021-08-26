48d7796827538c514e3a90f864954c3671547687
EXTERNAL Gonzalez Gomez Hugo Emmanuel (HCM <ghu1ga@bosch.com>
2021-08-26 15:30:44 -0500

    v1.2

--- js/app_2.js
+++ js/app_2.js
@@ -1,10 +1,33 @@
   //Funcion medida de la Figura Geometrica
 
-  document.getElementById("tipoFormula").addEvenListener("change", medidaFG);
+  function mFigura(){
+    var x = document.getElementById("tipoFormula").value; //valor obtenido
+    var y = document.getElementById("tipoFigura").value;
+    var z = docuemnt.getElementById("").value;
+    //evaluar el valor obtenido
+    if(x == "Area"){
+      //alert("Eres el Area")  
+      
+      if(y == "Cuadrado"){
+        //alert("Elegiste area del cuadrado")
+        document.getElementById("miImagenFigura").src = 'imgs/Square.png';
+        document.getElementById(mFormula):
 
-  function medidaFG(){
+      } else if(y == "Triangulo"){
+        document.getElementById("miImagenFigura").src = 'imgs/Triangle.png';
+      } else if(y == "Rectangulo"){
+        document.getElementById("miImagenFigura").src = 'imgs/Rectangle.png';
+      } else if(y == "Circulo"){
+        document.getElementById("miImagenFigura").src = 'imgs/Circle.png';
+      } else if(y == "Rombo"){
+        document.getElementById("miImagenFigura").src = 'imgs/Rhombus.png';
+      }
 
+    } else if(x == "Perimetro"){
 
-  }
+      //alert("Eres el Perimetro");
+    
+    }
 
+  } //Termino de la funcion
 
