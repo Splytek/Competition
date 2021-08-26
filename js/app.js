6aad493e7090776b86285a4e635d80ec8270b236
Hugo Emmanuel Gonzalez Gomez <splytek@gmail.com>
2021-08-26 09:45:22 -0500

    v1.2

--- js/app.js
+++ js/app.js
@@ -1,36 +1,11 @@
 function mostrarDatos(){
 
-    //var numeroBinarioTxt = document.getElementById("numero-Binario").value; //obtenemos el valor del input
+    var numeroBinarioObtenido = document.getElementById('numero-Binario').value;
+    
+    var Texto = "";
 
-    var numeroBinario = document.getElementById('numero-Binario').value;
+    Texto += numeroBinarioObtenido + "\n";
 
-    //numero 
-    var numeroC = parseInt(numeroBinario); 
-
-    //CODIGO ASCII
-    if(numeroC> String.fromCharCode(48) && String.fromCharCode(57)<numeroC){
-      alert("estas entre los numeros 0 y 9");
-    } else {
-      alert("No entras");
-    }
+    document.getElementById("mostrarTexto-Binarios").innerHTML = Texto+=Texto + "\n";
 
 }
-
-
-
-
-
-    
-/*     if(numeroBinarioText.length <= 15){
-      var texto = numeroBinarioText;
-      document.getElementById("mostrarTexto-Binarios").innerHTML = texto;
-    } else {
-      alert('Solo se permiten 15 numeros');    
-    } */
-
-
-/*     for(i=0; i<10; 1++){
-      if(){
-
-      }
-    } */
